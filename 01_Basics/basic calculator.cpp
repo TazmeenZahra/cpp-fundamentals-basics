0a810f1380ed15c4418a98b9d96b782325cbe0d9
Tazmeen Zahra <sofiasareem07@gmail.com>
2026-03-31 13:13:42 +0500

    Create basic calculator.cpp

---
+++ 01_Basics/basic calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+ float Nb_1 = 4;
+ float Nb_2 = 5;
+ int Nb_S = Nb_1 + Nb_2;
+ int Nb_Df = Nb_2 - Nb_1;
+ int Nb_P = Nb_1 * Nb_2;
+float Nb_D = Nb_2 / Nb_1;
+ 
+ cout << "The sum of the numbers is: " << Nb_S << endl;
+ cout << "The difference of the numbers is: " << Nb_Df << endl;
+ cout << "The product of the numbers is: " << Nb_P << endl;
+ cout << "The division of the numbers is: " << Nb_D << endl;
+ return 0;
+}
