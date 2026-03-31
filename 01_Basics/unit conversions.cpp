bb0e093fc137f19f63f77198b1402c35634e276c
Tazmeen Zahra <sofiasareem07@gmail.com>
2026-03-31 13:14:54 +0500

    Create unit conversions.cpp

---
+++ 01_Basics/unit conversions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+ float D_kilo = 10;
+ float D_mile = D_kilo * 0.621371;
+ 
+ cout << "Distance in kilometers: " << D_kilo << endl;
+ cout << "Distance in miles: " << D_mile;
+ return 0;
+}
