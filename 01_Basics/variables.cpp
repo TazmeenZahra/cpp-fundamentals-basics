b81bfc41de5207fe82a4855002474f9b453c3f78
Tazmeen Zahra <sofiasareem07@gmail.com>
2026-03-31 13:07:18 +0500

    Create variables.cpp

---
+++ 01_Basics/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int first_in = 25;
+float second_fp = 19.99;
+ string third_s = "Hello, C++!";
+ cout << "The value of the integer is: " << first_in << endl;
+ cout << "The value of the floating-point number is: " << second_fp << endl;
+ cout << "The value of the string is: " << third_s;
+ return 0;
+}
