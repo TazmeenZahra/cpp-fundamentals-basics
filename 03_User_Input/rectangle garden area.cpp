809fbe00d74cadf933589729bdf371d47ee5a75d
Tazmeen Zahra <sofiasareem07@gmail.com>
2026-03-31 13:29:10 +0500

    Create rectangle garden area.cpp

---
+++ 03_User_Input/rectangle garden area.cpp
@@ -0,0 +1,10 @@
+// Finding Area
+#include <iostream>
+using namespace std;
+int main() {
+    int length_in_meters = 10;
+    int width_in_meters = 5;
+    int area_in_metersquare = length_in_meters *width_in_meters;
+    cout << "The Area of rectangular garder is: " << area_in_metersquare;
+    return 0;
+}
